04ca614187e0c97f22e1da156f9727131b81c906
safarsafarov <safarov.safar@hotmail.com>
2021-05-28 21:13:48 +0800

    commit

---
+++ test/test/test/Home.swift
@@ -0,0 +1,22 @@
+//
+//  Home.swift
+//  test
+//
+//  Created by codestorm on 2021/5/2.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
---
+++ test/test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by codestorm on 2021/5/2.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
