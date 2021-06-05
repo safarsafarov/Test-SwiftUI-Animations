153f23a76f3ee122003540e76476414be9f13fef
safarsafarov <safarov.safar@hotmail.com>
2021-06-05 22:54:26 +0800

    commit

--- Navigation-Side/Navigation-Side/VIew/Home.swift
+++ Navigation-Side/Navigation-Side/VIew/Home.swift
@@ -11,13 +11,44 @@ struct Home: View {
     @Binding var selectedTab: String
     
     // Hiding Tab bar..
+    init(selectedTab: Binding<String>){
+        self._selectedTab = selectedTab
+        UITabBar.appearance().isHidden = true
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // Tab View With Tabs...
+        TabView(selection: $selectedTab){
+            HomePage()
+                .tag("Home")
+            History()
+                .tag("History")
+            Settings()
+                .tag("Settings")
+            Help()
+                .tag("Help")
+            Notification()
+                .tag("Notifications")
+        }
     }
 }
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
+    }
+}
+
+struct HomePage: View {
+    var body: some View {
+        navigationView{
+            VStack(alignment: .leading, spacing: 20){
+                Image("pic")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: getRect().width - 50, height: 400)
+                    .cornerRadius(20)
+            }
+        }
     }
 }
