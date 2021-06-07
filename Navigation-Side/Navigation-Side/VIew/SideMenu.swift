6de9c97e908f4ac4977c1f13ca8752581adb3b3a
safarsafarov <safarov.safar@hotmail.com>
2021-06-07 23:05:58 +0800

    commit

---
+++ Navigation-Side/Navigation-Side/VIew/SideMenu.swift
@@ -0,0 +1,20 @@
+//
+//  SideMenu.swift
+//  Navigation-Side
+//
+//  Created by codestorm on 2021/6/17.
+//
+
+import SwiftUI
+
+struct SideMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SideMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenu()
+    }
+}
