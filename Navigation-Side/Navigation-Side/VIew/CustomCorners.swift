f893fce4a29390864812794a0c08708db2f986ef
safarsafarov <safarov.safar@hotmail.com>
2021-06-11 16:36:31 +0800

    commit

---
+++ Navigation-Side/Navigation-Side/VIew/CustomCorners.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct CustomCorners: Shape {
+    var corners: UIRectCorner
+    var radius: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, cornerRadius: )
+    }
+}
