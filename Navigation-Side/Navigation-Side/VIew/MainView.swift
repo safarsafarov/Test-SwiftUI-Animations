858238fcfcca5c3973b862f7e299e2be1309ef13
safarsafarov <safarov.safar@hotmail.com>
2021-06-14 00:03:29 +0800

    commit

--- Navigation-Side/Navigation-Side/VIew/MainView.swift
+++ Navigation-Side/Navigation-Side/VIew/MainView.swift
@@ -1,28 +1,36 @@
-//
-//  MainView.swift
-//  Navigation-Side
-//
-//  Created by codestorm on 2021/6/17.
-//
-
 import SwiftUI
 
 struct MainView: View {
+    @State var selectedTab = "Home"
+    @State var showMenu = false
+    
     var body: some View {
-        ZStack{
+        ZStack {
             Color("blue")
                 .ignoresSafeArea()
+            // Side menu...
             
-            // Side Menu...
             ScrollView(getRect().height < 750 ? .vertical : .init(), showsIndicators: false, content: {
-                SudeMenu
+                SideMenu(selectedTab: $selectedTab)
+            })
+            
+            ZStack {
+                Color.white
+                    .opacity(0.5)
+                    .cornerRadius(showMenu ? 15 : 0)
+                // Shadow
+                    .shadow(color: Color.black.opacity(0.07), radius: 5, x: -5, y: 0)
+                    .offset(x: showMenu ? -25 : 0)
+                    .padding(.vertical, 30)
+                Color.white
+                    .opacity(0.4)
+                    .cornerRadius(showMenu ? 15 : 0)
+                // Shadow...
+                    .shadow(color: Color.black.opacity(0.07), radius: 5, x: -5, y: 0)
+                    .offset(x: showMenu ? -50 : 0)
+                    .padding(.vertical, 60)
             }
+            .scaleEffect()
         }
-    }
-}
-
-struct MainView_Previews: PreviewProvider {
-    static var previews: some View {
-        MainView()
     }
 }
--- Navigation-Side/Navigation-Side/VIew/TabButton.swift
+++ Navigation-Side/Navigation-Side/VIew/TabButton.swift
@@ -12,11 +12,36 @@ struct TabButton: View {
         Button(action: {
             withAnimation(.spring()){selectedTab = title}
         }, label: {
-            HStack(spacing: 15)
-            Image(systemName: image)
-            
-            
-            
+            HStack(spacing: 15) {
+                Image(systemName: image)
+                    .font(.title2)
+                    .frame(width: 30)
+                
+                Text(title)
+                    .fontWeight(.semibold)
+            }
+            .foregroundColor(selectedTab == title ? Color("blue") : .white)
+            .padding(.vertical, 12)
+            .padding(.horizontal, 10)
+            // Max Frame...
+            .frame(maxWidth: getRect().width - 170, alignment: .leading)
+            .background(
+                ZStack {
+                    if selectedTab == title {
+                        Color.white
+                            .opacity(selectedTab == title ? 1 : 0)
+                            .clipShape(CustomCorners(corners: [.topRight,.bottomRight], radius: 12))
+                            .matchedGeometryEffect(id: "TAB", in: animation)
+                    }
+                }
+            )
         })
+    }
+}
+
+
+struct TabButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
